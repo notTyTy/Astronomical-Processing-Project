b49d7414ef0ac2990be3e1ee4afa084f377722d8
Ash <ashy@tyty.cloud>
2024-04-28 18:26:21 +0800

    Changed Target Framework

---
+++ ConsoleServer/ConsoleServer/AstroServer.cs
@@ -0,0 +1,36 @@
+﻿using AstroMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleServer
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    internal class AstroServer : IAstroContract
+    {
+        StarCalculations starCalculations = new StarCalculations();
+
+        public double StarVelocity(double observed, double rest)
+        {
+            return starCalculations.StarVelocity(observed, rest);
+        }
+
+        public double StarDistance(double arcSecondsAngle)
+        {
+            return starCalculations.StarDistance(arcSecondsAngle);
+        }
+
+        public double TemperatureK(double celsius)
+        {
+            return starCalculations.TemperatureK(celsius);
+        }
+
+        public double EventHorizon(double mass)
+        {
+            return starCalculations.EventHorizon(mass);
+        }
+    }
+}
---
+++ ConsoleServer/ConsoleServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleServer
+{
+    internal class MyServerProgram
+    {
+        static void Main()
+        {
+            string address = "net.pipe://localhost/netpipeAstro";
+            ServiceHost serviceHost = new ServiceHost(typeof(AstroServer));
+            NetNamedPipeBinding binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            serviceHost.AddServiceEndpoint(typeof(IAstroContract), binding, address);
+            serviceHost.Open();
+            Console.WriteLine("ServiceHost is running. Press <<Return>> to Exit");
+            Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
